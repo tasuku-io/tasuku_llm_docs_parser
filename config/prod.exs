40990a1d4de74d5ed24b1170e89c64a0be7c1833
Luis Guzman <luisdaniel.guzmanp@gmail.com>
2025-07-16 01:36:35 +0900

    releases configuration

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tasuku_llm_docs_parser, TasukuLlmDocsParserWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
+
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: TasukuLlmDocsParser.Finch
